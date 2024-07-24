be048a043f06ec2c0f9aa5885ece54994fa1f732
Ignatiusdork <littignatius@gmail.com>
2024-07-24 16:40:55 +0100

    code-update

--- components/skills.tsx
+++ components/skills.tsx
@@ -29,7 +29,7 @@ function Skills() {
         id='skills'
         className='mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40'
     >
-        <SectionHeading>My skills</SectionHeading>
+        <SectionHeading>MY skills</SectionHeading>
         <ul className='flex flex-wrap justify-center gap-2
         text-lg text-gray-800'>
             {skillsData.map((skill, index) => (
